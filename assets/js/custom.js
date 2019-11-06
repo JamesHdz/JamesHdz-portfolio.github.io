886004ce62658ca824e7904b0207168977484689
James H <hernandez.jimmy@gmail.com>
2019-11-06 09:41:30 -0600

    Worked on navbar transparency

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,15 +2,15 @@
          * Transparent navbar brand
          /* ---------------------------------------------- */
 
-         function navbarAnimation(navbar, homeSection, navHeight) {
-            var topScroll = $(window).scrollTop();
-            if (navbar.length > 0 && homeSection.length > 0) {
-                if(topScroll >= navHeight) {
-                    navbar-custom.removeClass('brand-color');
-                    navbar-brand.removeClass('brand-color');
-                } else {
-                    navbar-custom.addClass('brand-color');
-                    navbar-brand.addClass('brand-color');
-                }
-            }
-        }
+        //  function navbarAnimation(navbar, homeSection, navHeight) {
+        //     var topScroll = $(window).scrollTop();
+        //     if (navbar.length > 0 && homeSection.length > 0) {
+        //         if(topScroll >= navHeight) {
+        //             navbar-custom.removeClass('brand-color');
+        //             navbar-brand.removeClass('brand-color');
+        //         } else {
+        //             navbar-custom.addClass('brand-color');
+        //             navbar-brand.addClass('brand-color');
+        //         }
+        //     }
+        // }
